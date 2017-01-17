23ceb33bf714c8b97bdd6fa1ed1a3f894601bc4f
Aidan Baack <aebaack@gmail.com>
2017-01-17 14:01:35 -0700

    Refactor code

--- caesarShift.js
+++ caesarShift.js
@@ -3,60 +3,72 @@
 function encodeString(plainStr) {
   return plainStr
     .split('')
-    .reduce((encodedStr, plainChar) => {
-      if (plainChar.match(/[A-z]/)) {
-        return encodedStr + encodeLetter(plainChar);
-      } else {
-        return encodedStr + plainChar;
-      }
-    }, '');
+    .reduce((encodedStr, char) => encodedStr + encodeLetter(char), '');
 }
 
 function encodeLetter(char) {
-  switch (char) {
-    case 'X':
-    case 'x':
-      return char === 'X' ? 'A' : 'a';
-    case 'Y':
-    case 'y':
-      return char === 'Y' ? 'B' : 'b';
-    case 'Z':
-    case 'z':
-      return char === 'Z' ? 'C' : 'c';
-    default:
-      return String.fromCharCode(char.charCodeAt() + 3);
+  if (!char.match(/[A-z]/)) {
+    return char;
   }
+  const wrappedCodes = ['X', 'Y', 'Z', 'x', 'y', 'z'];
+  return wrappedCodes.includes(char) ?
+    String.fromCharCode(char.charCodeAt() - 23) :
+    String.fromCharCode(char.charCodeAt() + 3);
 }
 
 function decodeString(encodedStr) {
   return encodedStr
     .split('')
-    .reduce((decodedStr, encodedChar) => {
-      if (encodedChar.match(/[A-z]/)) {
-        return decodedStr + decodeLetter(encodedChar);
-      } else {
-        return decodedStr + encodedChar;
-      }
-    }, '');
+    .reduce((decodedStr, char) => decodedStr + decodeLetter(char), '');
 }
 
 function decodeLetter(char) {
-  switch (char) {
-    case 'A':
-    case 'a':
-      return char === 'A' ? 'X' : 'x';
-    case 'B':
-    case 'b':
-      return char === 'B' ? 'Y' : 'y';
-    case 'C':
-    case 'c':
-      return char === 'C' ? 'Z' : 'z';
-    default:
-      return String.fromCharCode(char.charCodeAt() - 3);
+  if (!char.match(/[A-z]/)) {
+    return char;
   }
+  const wrappedCodes = ['A', 'B', 'C', 'a', 'b', 'c'];
+  return wrappedCodes.includes(char) ?
+    String.fromCharCode(char.charCodeAt() + 23) :
+    String.fromCharCode(char.charCodeAt() - 3);
 }
 
 module.exports = {
   encode: encodeString,
   decode: decodeString
 };
+
+
+
+// Old encode
+// switch (char) {
+//   case 'X':
+//   case 'x':
+//     return char === 'X' ? 'A' : 'a';
+//   case 'Y':
+//   case 'y':
+//     return char === 'Y' ? 'B' : 'b';
+//   case 'Z':
+//   case 'z':
+//     return char === 'Z' ? 'C' : 'c';
+//   default:
+//     return char.match(/[A-z]/) ? 
+//       String.fromCharCode(char.charCodeAt() + 3) :
+//       char;
+// }
+
+// Old decode
+// switch (char) {
+//   case 'A':
+//   case 'a':
+//     return char === 'A' ? 'X' : 'x';
+//   case 'B':
+//   case 'b':
+//     return char === 'B' ? 'Y' : 'y';
+//   case 'C':
+//   case 'c':
+//     return char === 'C' ? 'Z' : 'z';
+//   default:
+//     return char.match(/[A-z]/) ?
+//       String.fromCharCode(char.charCodeAt() - 3) :
+//       char;
+//}
