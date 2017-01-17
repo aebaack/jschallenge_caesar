1c6024c95e42df287b397627f152777489b4aeba
Aidan Baack <aebaack@gmail.com>
2017-01-17 13:21:57 -0700

    Complete first solution for encoding

--- caesarShift.js
+++ caesarShift.js
@@ -1,4 +1,39 @@
+'use strict';
 
+function encodeString(plainStr) {
+  return plainStr
+    .split('')
+    .reduce((encodedStr, plainChar) => {
+      if (plainChar.match(/[A-z]/)) {
+        return encodedStr + encodeLetter(plainChar);
+      } else {
+        return encodedStr + plainChar;
+      }
+    }, '');
+}
 
+function encodeLetter(char) {
+  switch (char) {
+    case 'X' || 'x':
+      return char === 'X' ? 'A' : 'a';
+    case 'Y' || 'y':
+      return char === 'Y' ? 'B' : 'b';
+    case 'Z' || 'z':
+      return char === 'Z' ? 'C' : 'c';
+    default:
+      return String.fromCharCode(char.charCodeAt() + 3);
+  }
+}
 
-module.exports = {}
+function decodeString(encodedStr) {
+
+}
+
+function decodeLetter(char) {
+  
+}
+
+module.exports = {
+  encode: encodeString,
+  decode: decodeString
+};
